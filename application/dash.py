8e27dd0b6448554597bf6f7b39ab877606d6996e
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-06-22 10:41:43 -0500

    Create dash.py

---
+++ application/dash.py
@@ -0,0 +1,22 @@
+# violencia de genero distinta a violencia familiar
+
+import dash
+import matplotlib.pyplot as plt 
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+import pandas as pd
+import plotly.graph_objs as go
+from dash.dependencies import Input, Output
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.io as pio
+import numpy as np
+import dash_table
+import sidetable as stb
+import datetime
+from datetime import datetime, timedelta
+from datetime import date
+import geopandas as gpd
+import flask
+import os
