0c4b74e94a950730327206e9772ef44e468415a7
Felipe de Alba <45267327+fdealbam@users.noreply.github.com>
2021-09-14 11:19:18 -0500

    Update dash.py

--- application/dash.py
+++ application/dash.py
@@ -402,7 +402,7 @@ body = html.Div([
                     "agenda legislativa, pero hoy alcanzan relevancia en la agenda seguridad pública del país, también. "
                    " Entre 2015 y 2021 se registraron "+ str(f"{int(TOTVGDISTFAM):,}") +" casos, lo que representa una tasa de "+
        str(TASAVGDISTFAM) +" delitos por cada 100 mil habitantes. "+
-                    "Este dashboard analítico se compone de una sección en la cual tratamos la violencia de género en todas sus modalidades distinta a la violencia familiar, observamos "
+                    "Este tablero analítico se compone de una sección en la cual tratamos la violencia de género en todas sus modalidades distinta a la violencia familiar, observamos "
                     "su gravedad según intervalos anuales o mensuales; incluimos el análisis detallado de cuatro "
                     "entidades con más incidencias de este delito; finalmente, comparamos los rankings por entidad "
                     "según sumas del periódo 2015 al 2021 con las tasas por entidad del mismo intervalo. " 
@@ -715,14 +715,14 @@ body = html.Div([
                 html.Br(),
                 html.H4("Metodología "),
                 html.P(
-                    "El presente dashboard es un ejercicio institucional con el objeto de "
+                    "El presente tablero es un ejercicio institucional con el objeto de "
                     "informar a las diputadas y diputados y público interesado sobre un tema "
                     "de vital importancia en la vida política. "
                     "La metodología que hemos empleado para analizar los datos la detallamos enseguida. "
                     "Como se indica en cada caso, la información sobre el delito violencia de género en todas sus modalidades distinta a la violencia familiar proviene del Secretariado "
                     "Ejecutivo Nacional del Sistema Nacional de Seguridad Pública (SENSNSP) (2015-2021); "
                     " "
-                    "Este dashboard seguramente será completado progresivamente con otras fuentes de información "
+                    "Este tablero seguramente será completado progresivamente con otras fuentes de información "
                     "tanto gubernamental, como aquella proveniente de organizaciones civiles que " 
                     "dan seguimiento al tema. "
                     "En ningún caso, este contenido representa algún "
